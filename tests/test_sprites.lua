0c485ad10e3e00c29f817dde14143cc1e5e03bba
Casey Parker <02-germane-atomic@icloud.com>
2025-09-23 09:26:41 -0700

    testing stuff

--- tests/test_sprites.lua
+++
@@ -1,9 +0,0 @@
--- Example test module for sprites
--- Add tests here for sprite-related functionality
--- This is loaded by test.lua for TDD
-
--- Example: Test sprite initialization
--- (Add to the frame callback or as separate logic)
-
--- For now, placeholder
-print("Loaded test_sprites.lua")
